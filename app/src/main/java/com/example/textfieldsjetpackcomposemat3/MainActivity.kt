a9a174e03228056f5130cb779d6b589a8c0df8e4
Stefan Bayne <stefanbusiness95@gmail.com>
2023-09-08 19:39:52 -0400

    adjusting compile versions and basic textfield.

--- app/src/main/java/com/example/textfieldsjetpackcomposemat3/MainActivity.kt
+++ app/src/main/java/com/example/textfieldsjetpackcomposemat3/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.textfieldsjetpackcomposemat3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.textfieldsjetpackcomposemat3.ui.theme.TextfieldsJetpackComposeMat3Theme
@@ -25,8 +33,19 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Column(
                         modifier = Modifier.fillMaxSize(),
-
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ) {
+                        var normalFieldText by remember {
+                            mutableStateOf("")
+                        }
+                        TextField(
+                            value = normalFieldText,
+                            onValueChange = { normalFieldText = it }
+                        )
+//                        OutlinedTextField(
+//                            value = ,
+//                            onValueChange = )
                     }
                 }
             }
